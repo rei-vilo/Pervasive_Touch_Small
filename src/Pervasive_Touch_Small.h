d0577a23dab08d0d2b6d69e4c237dcd3e7c0d58c
Rei Vilo <rei-vilo@users.noreply.github.com>
2026-02-19 16:13:17 +0100

    Updated year to 2026

--- src/Pervasive_Touch_Small.h
+++ src/Pervasive_Touch_Small.h
@@ -10,7 +10,7 @@
 /// @date 21 May 2025
 /// @version 909
 ///
-/// @copyright (c) Pervasive Displays Inc., 2021-2025
+/// @copyright (c) Pervasive Displays Inc., 2021-2026
 /// @copyright All rights reserved
 /// @copyright For exclusive use with Pervasive Displays screens
 ///
